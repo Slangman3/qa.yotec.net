e7c8a3d1c9725029f5b61293d3217dd29efc8674
Slangman3 <slangman3@gmail.com>
2019-09-06 18:56:04 +0300

    Added POM and test for jumping on nav menu, but still needed to add method check breadcrumbs and URL, then go back on main page.

---
+++ src/test/java/tests/NavigationThroughTest.java
@@ -0,0 +1,34 @@
+package tests;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import pages.YotecMainPage;
+
+import java.util.concurrent.TimeUnit;
+
+public class NavigationThroughTest {
+    ChromeDriver driver;
+
+    @BeforeClass
+    public void openDriver() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/webdrivers/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void navigationThroughMenu() {
+        YotecMainPage yotec = new YotecMainPage(driver);
+        yotec.wabashMenu(7);
+//        yotec.innovationMenu(3);
+
+    }
+
+    @AfterClass
+    public void closeDriver() {
+        driver.quit();
+    }
+}
