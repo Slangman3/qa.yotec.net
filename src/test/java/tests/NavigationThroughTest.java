f160fa1736d5e8f913b86b563bdf2c3bd3f41f39
Slangman3 <slangman3@gmail.com>
2019-09-10 01:08:52 +0300

    Added POM on dealer menu, also added tests on it.

--- src/test/java/tests/NavigationThroughTest.java
+++ src/test/java/tests/NavigationThroughTest.java
@@ -4,7 +4,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import pages.DealerMainPage;
 import pages.YotecMainPage;
+
 import java.util.concurrent.TimeUnit;
 
 public class NavigationThroughTest {
@@ -23,7 +25,35 @@ public class NavigationThroughTest {
         YotecMainPage yotec = new YotecMainPage(driver);
         yotec.wabashMenu(7);
         yotec.innovationMenu(3);
+        yotec.ourProductsMenu();
+        yotec.ourBrandsMenu(6);
+        yotec.workWithWabashMenu(0);
+        yotec.timeLineMenu();
+        yotec.locationSearchMenu(2);
+//        yotec.openPage
+//                .selectMenuItem("MenuItem Nmae")
+//                .checkThatCOntainsItems("Submenu 1", "submenu 2")
+    }
 
+    @Test
+    public void dealerSearchTruckOnTest() {
+        DealerMainPage dealerSearchInputPage = new DealerMainPage(driver);
+        dealerSearchInputPage.openPage()
+                .enterSearchCriteria("Truck")
+                .waitUntilLoadResultList()
+                .validateNumberOfItems("49 results near Indiana", 49)
+                .selectTypeOfLocation("Warranty")
+                .waitUntilLoadResultList()
+                .validateAfterCheckBoxing("31 results near Indiana", 31);
+    }
+
+    @Test
+    public void locationTypeOnTest() {
+        DealerMainPage dealerCheckBoxPage = new DealerMainPage(driver);
+        dealerCheckBoxPage.openPage()
+                .selectTypeOfLocation("Warranty")
+                .waitUntilLoadResultList()
+                .validateAfterCheckBoxing("48 results near Indiana", 48);
     }
 
     @AfterClass
